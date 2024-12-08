bc3d9063f4f16bcc4adbd9fd1bbbaff548c0b076
PrathyushY <prathyushyeturi@gmail.com>
2024-12-08 11:54:51 -0500

    Deleted everything in main and just added generic tele op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GenericTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GenericTeleOp.java
@@ -1,13 +1,14 @@
-package org.firstinspires.ftc.teamcode.config.testers;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
-public class DemoTeleOp extends LinearOpMode {
+public class GenericTeleOp extends LinearOpMode {
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
         // Declare our motors
         // Make sure your ID's match your configuration
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
@@ -19,20 +20,20 @@ public class DemoTeleOp extends LinearOpMode {
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        //frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        //backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = Math.pow(-gamepad1.left_stick_y,3); // Remember, Y stick value is reversed
-            double x = Math.pow(gamepad1.left_stick_x * 1.1,3); // Counteract imperfect strafing
-            double rx = Math.pow(gamepad1.right_stick_x,3);
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
 
             // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,x
+            // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/utils/RunMode.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.config.utils;
-
-public enum RunMode {
-    AUTO,TELEOP,TEST
-}
