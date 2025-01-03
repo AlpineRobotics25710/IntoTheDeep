42295a8b9458ee96b3a6b677d4ff1bb52aa8698f
PrathyushY <prathyushyeturi@gmail.com>
2025-01-03 09:26:36 -0500

    Coded the intake command. Why isn't this better than using set intake? commands shouldn't really be general purpose. they should just execute a single action of the robot. at least that is my understanding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/IntakeCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/IntakeCommand.java
@@ -2,27 +2,46 @@ package org.firstinspires.ftc.teamcode.robot.commands;
 
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
 
 import org.firstinspires.ftc.teamcode.robot.Robot;
 import org.firstinspires.ftc.teamcode.robot.mechanisms.intake.Extendo;
 import org.firstinspires.ftc.teamcode.robot.mechanisms.intake.IntakeArm;
+import org.firstinspires.ftc.teamcode.robot.mechanisms.intake.IntakeEnd;
 
 /**
  * Gets ready to intake a game piece
  */
-public class IntakeCommand extends ParallelCommandGroup {
-    private final Robot robot;
+public class IntakeCommand extends SequentialCommandGroup {
+    private final IntakeEnd intakeEnd;
+    private final IntakeArm intakeArm;
+    private final Extendo extendo;
+    private static final long ARM_WAIT_TIME = 750;
+    private static final long ACTIVE_WAIT_TIME = 300;
 
     public IntakeCommand(Robot robot) {
-        this.robot = robot;
+        intakeEnd = robot.intakeEnd;
+        intakeArm = robot.intakeArm;
+        extendo = robot.extendo;
         addRequirements(robot.intakeArm, robot.extendo);
     }
 
     @Override
     public void initialize() {
         addCommands(
-                new InstantCommand(() -> robot.extendo.setTargetPosition(Extendo.MAX_LENGTH)),
-                new InstantCommand(() -> robot.intakeArm.setState(IntakeArm.IntakeArmState.INTAKE))
+                new ParallelCommandGroup(
+                        new InstantCommand(() -> extendo.setTargetPosition(Extendo.MAX_LENGTH)),
+                        new InstantCommand(() -> intakeArm.setState(IntakeArm.IntakeArmState.INTAKE))
+                ),
+                new WaitCommand(ARM_WAIT_TIME),
+                new InstantCommand(() -> intakeEnd.setState(IntakeEnd.ActiveState.FORWARD)),
+                new WaitCommand(ACTIVE_WAIT_TIME),
+                new ParallelCommandGroup(
+                        new InstantCommand(() -> intakeEnd.setState(IntakeEnd.ActiveState.OFF)),
+                        new InstantCommand(() -> intakeArm.setState(IntakeArm.IntakeArmState.TRANSFER)),
+                        new InstantCommand(() -> extendo.setTargetPosition(Extendo.BASE_POS))
+                )
         );
     }
 }
