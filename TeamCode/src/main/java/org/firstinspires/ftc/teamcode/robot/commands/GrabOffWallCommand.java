f903f6fda4f1e8017e302ecd3176c3943188f24d
PrathyushY <prathyushyeturi@gmail.com>
2025-01-04 08:36:48 -0500

    forget what happened in the last commit i was being dumb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/GrabOffWallCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/GrabOffWallCommand.java
@@ -7,23 +7,21 @@ import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitCommand;
 
 import org.firstinspires.ftc.teamcode.robot.Robot;
-import org.firstinspires.ftc.teamcode.robot.mechanisms.intake.IntakeEnd;
 import org.firstinspires.ftc.teamcode.robot.mechanisms.outtake.OuttakeArm;
+import org.firstinspires.ftc.teamcode.robot.mechanisms.outtake.OuttakeClaw;
 import org.firstinspires.ftc.teamcode.robot.mechanisms.outtake.OuttakeSlides;
 
 @Config
 public class GrabOffWallCommand extends SequentialCommandGroup {
     public static long slidesTimeout = 500;
-    public static long activeTimeout = 500;
 
     public GrabOffWallCommand(Robot robot) {
         super(
                 new InstantCommand(() -> robot.outtakeSlides.setTargetPosition(OuttakeSlides.GRAB_OFF_WALL)),
                 new WaitCommand(slidesTimeout),
                 new InstantCommand(() -> robot.outtakeArm.setState(OuttakeArm.OuttakeArmState.WALL_INTAKE)),
-                new InstantCommand(() -> robot.intakeEnd.setState(IntakeEnd.ActiveState.FORWARD)),
-                new WaitCommand(activeTimeout),
-                new InstantCommand(() -> robot.intakeEnd.setState(IntakeEnd.ActiveState.OFF)),
+                new InstantCommand(() -> robot.outtakeClaw.setClawState(OuttakeClaw.OuttakeClawState.OPEN)),
+                new InstantCommand(() -> robot.outtakeClaw.setClawState(OuttakeClaw.OuttakeClawState.CLOSED)),
                 new ParallelCommandGroup(
                         new InstantCommand(() -> robot.outtakeArm.setState(OuttakeArm.OuttakeArmState.TRANSFER)),
                         new InstantCommand(() -> robot.outtakeSlides.setTargetPosition(OuttakeSlides.TRANSFER_POS))
