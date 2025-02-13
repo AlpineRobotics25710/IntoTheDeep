72675ad47e3f43620a847dee3162d6564e5dea06
Hypothetical <adi.varshney09@gmail.com>
2025-02-12 19:20:30 -0500

    Revert "bro what"
    This reverts commit 5d2a29fe11a18a4f29e3cd333b61fb7bdea39af4.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/OuttakeRetractCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/OuttakeRetractCommand.java
@@ -5,13 +5,10 @@ import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitCommand;
 
 import org.firstinspires.ftc.teamcode.robot.Robot;
-import org.firstinspires.ftc.teamcode.robot.commands.subsystemcommand.ExtendoCommand;
 import org.firstinspires.ftc.teamcode.robot.commands.subsystemcommand.OuttakeArmCommand;
 import org.firstinspires.ftc.teamcode.robot.commands.subsystemcommand.OuttakeClawCommand;
 import org.firstinspires.ftc.teamcode.robot.commands.subsystemcommand.OuttakeSlidesCommand;
 import org.firstinspires.ftc.teamcode.robot.commands.subsystemcommand.SwivelCommand;
-import org.firstinspires.ftc.teamcode.robot.mechanisms.intake.Extendo;
-import org.firstinspires.ftc.teamcode.robot.mechanisms.intake.IntakeArm;
 import org.firstinspires.ftc.teamcode.robot.mechanisms.outtake.OuttakeArm;
 import org.firstinspires.ftc.teamcode.robot.mechanisms.outtake.OuttakeClaw;
 import org.firstinspires.ftc.teamcode.robot.mechanisms.outtake.OuttakeSlides;
@@ -24,11 +21,10 @@ public class OuttakeRetractCommand extends SequentialCommandGroup {
                 new OuttakeClawCommand(robot, OuttakeClaw.OuttakeClawState.OPEN),
                 new WaitCommand(200),
                 new InstantCommand(() -> {
-                    if(robot.outtakeSlides.getCurrentPosition() > 500 || robot.extendo.getTargetPosition() == Extendo.MAX_LENGTH){
+                    if(robot.outtakeSlides.getCurrentPosition() > 500){
                         new WaitCommand(500);
                     }}
                 ),
-                new IntakeCommand(robot, 0, IntakeArm.IntakeArmState.INTERIM),
                 new OuttakeSlidesCommand(robot, OuttakeSlides.TRANSFER_POS)
         );
     }
